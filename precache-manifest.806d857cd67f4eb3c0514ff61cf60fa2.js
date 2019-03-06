04afd979fecc9689117fffd3ae1ff6453ac3ad00
Motohiko Makino <motohiko.makino@gmail.com>
2019-03-05 18:52:38 -0800

    Updates

---
+++ precache-manifest.806d857cd67f4eb3c0514ff61cf60fa2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clicky-game/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7985e359b3121cf81761",
+    "url": "/clicky-game/static/js/runtime~main.7985e359.js"
+  },
+  {
+    "revision": "d6d5477625b67a862675",
+    "url": "/clicky-game/static/js/main.d6d54776.chunk.js"
+  },
+  {
+    "revision": "646f3d2a74bef00b3a8f",
+    "url": "/clicky-game/static/js/2.646f3d2a.chunk.js"
+  },
+  {
+    "revision": "d6d5477625b67a862675",
+    "url": "/clicky-game/static/css/main.03126120.chunk.css"
+  },
+  {
+    "revision": "b33993783800df22bf653c76644be091",
+    "url": "/clicky-game/index.html"
+  }
+];
